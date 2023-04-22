64182449540322ead7a4c797880cbaa266062837
skjandu <skjandu@ucsd.edu>
2023-04-22 14:03:11 -0700

    started lab2

---
+++ lab2/StringServer.java
@@ -0,0 +1,32 @@
+package lab2;
+import java.io.IOException;
+import java.net.URI;
+
+class Handler implements URLHandler {
+    // The one bit of state on the server: a number that will be manipulated by
+    // various requests.
+    String message = "";
+
+    public String handleRequest(URI url) {
+        String[] parameters = url.getQuery().split("=");
+        if (url.getPath().equals("/add-message") && parameters[0].equals("s")) {
+            return message += (parameters[1] + "\n");
+        } 
+        else {
+            return "404 Not Found!";
+        }
+    }
+}
+
+class StringServer {
+    public static void main(String[] args) throws IOException {
+        if(args.length == 0){
+            System.out.println("Missing port number! Try any number between 1024 to 49151");
+            return;
+        }
+
+        int port = Integer.parseInt(args[0]);
+
+        Server.start(port, new Handler());
+    }
+}
